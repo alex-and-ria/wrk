748ccfdba37dbd437065dba43e77dc3e53a58856
alex-and-ria <alex-and-ria@meta.ua>
2018-02-13 22:28:48 +0200

    13.02.2018/22:28

--- snfr.c
+++ snfr.c
@@ -34,8 +34,8 @@ typedef union uint_u{
 	unsigned char u_byte[4];
 } uint_f;
 
-unsigned int getln(unsigned char* g_ip, uint_f* g_ipcnt,FILE *logfile){
-	unsigned char strff[line_len]; unsigned int cpcnt=0,i=0;
+unsigned int getln(unsigned char* strff,unsigned char* g_ip, uint_f* g_ipcnt,FILE *logfile){
+	unsigned int cpcnt=0,i=0;
 	fread(strff,sizeof(unsigned char),line_len,logfile);
 	for(i=0;i<m_ip_len;i++){
 		if(strff[i]!=' '){
@@ -45,17 +45,17 @@ unsigned int getln(unsigned char* g_ip, uint_f* g_ipcnt,FILE *logfile){
 	}
 	memcpy(g_ip+cpcnt,"\0",1);
 	g_ipcnt->u_byte[0]=strff[16]; g_ipcnt->u_byte[1]=strff[17]; g_ipcnt->u_byte[2]=strff[18]; g_ipcnt->u_byte[3]=strff[19];
-	printf("\ncpcnt=%d strlen(g_ip)=%d g_ip=%s",cpcnt,strlen(g_ip),g_ip);
+	printf("\n g_ip=%s g_ipcnt->u_val=%d",/*cpcnt,strlen(g_ip),*/g_ip,g_ipcnt->u_val);
 	return cpcnt;
 }
 
 int ffindip(struct sockaddr_in src,FILE *logfile){//it would be easier to work with data loaded to RAM (with list or so) but as
 //collected information may become too big to handle it, there used file to save data;
 	sps();
-	unsigned char str_to_file[line_len],g_ip[m_ip_len+1];
+	unsigned char str_to_file[line_len],strff[line_len],g_ip[m_ip_len+1];
 	uint_f curr_ipcnt,g_ipcnt; curr_ipcnt.u_val=1;
-	unsigned int i,cmpres; unsigned long int n_lines=0,sln=0,eln=0;
-	for(i=0;i<21;i++)
+	unsigned int i,cmpres; unsigned long int n_lines=0,sln=0,eln=0,curr_dif,curr_line; long int i_signed;
+	for(i=0;i<line_len;i++)
 		str_to_file[i]=' ';
 	memcpy(str_to_file+15-strlen(inet_ntoa(src.sin_addr)),inet_ntoa(src.sin_addr),strlen(inet_ntoa(src.sin_addr)));
 	memcpy(str_to_file+16,curr_ipcnt.u_byte,sizeof(curr_ipcnt.u_val));
@@ -67,85 +67,106 @@ int ffindip(struct sockaddr_in src,FILE *logfile){//it would be easier to work w
 		printf("\nfseek_err: %s",strerror(errno)); return -1;
 	}
 	unsigned long int send=ftell(logfile);
-	rewind(logfile); unsigned char ufchar;
+	rewind(logfile);// unsigned char ufchar;
 	printf("send-sstrt=%ld",send-sstrt); sps();
 	if((send-sstrt)==0){//file is empty;
 		rewind(logfile);//to be sure;
 		fwrite(str_to_file,sizeof(unsigned char),line_len,logfile);
 		return 0;
 	}
-	n_lines=(send-sstrt)/21; sln=0;eln=n_lines;
+	n_lines=(send-sstrt)/line_len; sln=0;eln=n_lines-1;
 	printf("n_lines=%ld",n_lines); sps();
 	
 	rewind(logfile);
-	while((eln-sln)>0){
-		unsigned int f_ip_len=getln(g_ip,&g_ipcnt,logfile);
-		printf("\ng_ip=%s g_ipcnt.u_val=%d",g_ip,g_ipcnt.u_val);
+	while((eln-sln)>1){
+		sps();
+		curr_dif=(unsigned int)((eln-sln)/2.+0.5); curr_line=sln+curr_dif;
+		fseek(logfile, curr_line*line_len,SEEK_SET);//find line position;
+		printf("\nsln=%ld, curr_dif=%ld curr_line=%ld pos=%ld",sln,curr_dif,curr_line,ftell(logfile));
+		unsigned int f_ip_len=getln(strff,g_ip,&g_ipcnt,logfile);
 		sps();
 		if((cmpres=strcmp(g_ip,inet_ntoa(src.sin_addr)))==0){//equal
-			//cnt++; put recort to file;
+			g_ipcnt.u_val++;
+			memcpy(strff+16,g_ipcnt.u_byte,sizeof(g_ipcnt.u_val));
+			fseek(logfile, curr_line*line_len,SEEK_SET);//find line position;
+			fwrite(strff,sizeof(unsigned char),line_len,logfile);
+			printf("\ncmpres=%d",cmpres);
+			return 0;
 		}
 		else if(cmpres<0){
-			sln=(eln-sln-0.5)/2;
+			sln=curr_line;
+			printf("\ncmpres=%d sln=%ld",cmpres,sln);
 		}
 		else{
-			eln=(eln-sln+0.5)/2;
+			eln=curr_line;
+			printf("\ncmpres=%d eln=%ld",cmpres,eln);
 		}
-		
-		//
+	}//we have src.sin_addr, sln, eln;
+	fseek(logfile, sln*line_len,SEEK_SET);//find start line position;
+	getln(strff,g_ip,&g_ipcnt,logfile);
+	cmpres=strcmp(g_ip,inet_ntoa(src.sin_addr)); printf("\ncmpres (sln,ni)=%d", cmpres);
+	if((cmpres=strcmp(g_ip,inet_ntoa(src.sin_addr)))==0){
+		printf("\n(sln=%ld) cmpres=%d g_ip=%s inet_ntoa(src.sin_addr))=%s",sln,cmpres,g_ip,inet_ntoa(src.sin_addr));
+		g_ipcnt.u_val++;
+		memcpy(strff+16,g_ipcnt.u_byte,sizeof(g_ipcnt.u_val));
+		fseek(logfile, sln*line_len,SEEK_SET);//find line position;
+		fwrite(strff,sizeof(unsigned char),line_len,logfile);
+		return 0;
 	}
-	/*
-	
-	if(ufchar=='|'){
-		rewind(logfile);//find file's beginning;
-		while((send-sstrt)/2>0){
-			printf("\n\nsend=%ld sstrt=%ld",send,sstrt);
-			if(fseek(logfile,(send-sstrt)/2,SEEK_SET)){//find middle of the file;
-				printf("\nfseek_err: %s",strerror(errno)); return -1;
-			}
-			else{
-				ufchar=getc(logfile); unsigned long int fcur_pos=ftell(logfile);
-				while(ufchar!='|'){//find line beginning;
-					printf("\nfcur_pos=%ld ufchar=%c",fcur_pos,ufchar);
-					fcur_pos-=1;
-					fseek(logfile,fcur_pos,SEEK_SET);
-					ufchar=getc(logfile);
-					if(fcur_pos<0){
-						printf("\n\n!!! fcur_pos<0");
-						return -1;
-					}					
-				}
-				fcur_pos=ftell(logfile);printf("\nfcur_pos=%ld",fcur_pos);
-				unsigned char ipstr[2*sizeof(inet_ntoa(src.sin_addr))]; unsigned long int ipcnt; int cmpres;
-				fscanf(logfile,"%s %ld\n",ipstr,&ipcnt);
-				printf("\nipstr=%s to %s ipcnt=%ld\n",ipstr,inet_ntoa(src.sin_addr),ipcnt);
-				if((cmpres=strcmp(ipstr,inet_ntoa(src.sin_addr)))==0 ){//equal 
-					ipcnt+=1;
-					printf("\nfcur_pos=%ld",fcur_pos);
-					fseek(logfile,fcur_pos-1,SEEK_SET);
-					fprintf(logfile,"|%s %ld\n",inet_ntoa(src.sin_addr),ipcnt);
-					printf("\nipcnt=%ld\n",ipcnt);
-					return 0;
-				}
-				else if(cmpres<0){
-					sstrt=(send-sstrt)/2;
-					printf("\n\ncmpres<0 send=%ld sstrt=%ld",send,sstrt);
-				}
-				else{
-					send=(send-sstrt)/2;
-					printf("\n\ncmpres>0 send=%ld sstrt=%ld",send,sstrt);
-				}				
-			}
+	else if(cmpres>0){//insert line before sln;
+		for(i_signed=n_lines-1;i_signed>=(long int)sln;i_signed--){//shift lines;
+			printf("\nib i_signed=%ld, n_lines=%ld, sln=%ld",i_signed,n_lines,sln);
 			sps();
+			fseek(logfile, i_signed*line_len,SEEK_SET);//find line position;
+			getln(strff,g_ip,&g_ipcnt,logfile);
+			fwrite(strff,sizeof(unsigned char),line_len,logfile);
 		}
-		printf("\nfcur_pos (ftell)=%ld",ftell(logfile));
-		fprintf(logfile,"|%s %ld\n",inet_ntoa(src.sin_addr), (unsigned long int)1);
-		sps();
+		printf("\n((b)sln=%ld) cmpres=%d g_ip=%s inet_ntoa(src.sin_addr))=%s",sln,cmpres,g_ip,inet_ntoa(src.sin_addr)); sps();
+		fseek(logfile, sln*line_len,SEEK_SET);
+		fwrite(str_to_file,sizeof(unsigned char),line_len,logfile);
 		return 0;
 	}
 	else{
-		return -1;
-	}*/
+		fseek(logfile, eln*line_len,SEEK_SET);
+		getln(strff,g_ip,&g_ipcnt,logfile);
+		if((cmpres=strcmp(g_ip,inet_ntoa(src.sin_addr)))==0){
+			printf("\n(eln=%ld) cmpres=%d g_ip=%s inet_ntoa(src.sin_addr))=%s",eln,cmpres,g_ip,inet_ntoa(src.sin_addr));
+			g_ipcnt.u_val++;
+			memcpy(strff+16,g_ipcnt.u_byte,sizeof(g_ipcnt.u_val));
+			fseek(logfile, eln*line_len,SEEK_SET);
+			fwrite(strff,sizeof(unsigned char),line_len,logfile);
+			return 0;
+		}
+		else if(cmpres>0){//insert line between sln and eln;
+			printf("\ncmpres (eln,ni)=%d", cmpres);
+			for(i_signed=n_lines-1;i_signed>=(long int)eln;i_signed--){
+				printf("\nibw i_signed=%ld, n_lines=%ld, eln=%ld",i_signed,n_lines,eln);
+				sps();
+				fseek(logfile, i_signed*line_len,SEEK_SET);//find line position;
+				getln(strff,g_ip,&g_ipcnt,logfile);
+				fwrite(strff,sizeof(unsigned char),line_len,logfile);
+			}
+			printf("\n(eln=%ld sln=%ld) cmpres=%d g_ip=%s inet_ntoa(src.sin_addr))=%s",eln,sln,cmpres,g_ip,inet_ntoa(src.sin_addr)); sps();
+			fseek(logfile, eln*line_len,SEEK_SET);
+			fwrite(str_to_file,sizeof(unsigned char),line_len,logfile);
+			return 0;
+		}
+		else{//insert line after eln;
+			printf("\ncmpres (eln,ni)=%d", cmpres);
+			for(i_signed=n_lines-1;i_signed>(long int)eln;i_signed--){
+				printf("\niaeln i_signed=%ld, n_lines=%ld, eln=%ld",i_signed,n_lines,eln);
+				sps();
+				fseek(logfile, i_signed*line_len,SEEK_SET);//find line position;
+				getln(strff,g_ip,&g_ipcnt,logfile);
+				fwrite(strff,sizeof(unsigned char),line_len,logfile);
+			}
+			printf("\n((a)eln=%ld) cmpres=%d g_ip=%s inet_ntoa(src.sin_addr))=%s",eln,cmpres,g_ip,inet_ntoa(src.sin_addr));
+			fseek(logfile, (eln+1)*line_len,SEEK_SET);
+			fwrite(str_to_file,sizeof(unsigned char),line_len,logfile);
+			return 0;
+		}
+	}
+	return -1;
 }
 
 void pprocess(unsigned char* buffer, int data_sz, struct ifreq ifr,FILE *logfile){
@@ -166,7 +187,7 @@ void pprocess(unsigned char* buffer, int data_sz, struct ifreq ifr,FILE *logfile
  
 int main(){
 	FILE *logfile; int saddr_size , data_size; struct sockaddr saddr; int dcnt=50;
-	char* if_name="eth0"; char fname[sizeof("log")+10+sizeof(".txt")]; 
+	char* if_name="wlan0"; char fname[sizeof("log")+10+sizeof(".txt")]; 
 	memcpy(fname,"log",strlen("log"));
 	memcpy(fname+strlen("log"),if_name,strlen(if_name));
 	memcpy(fname+strlen("log")+strlen(if_name),".txt\0",strlen(".txt\0")+1); printf("%s",fname);
